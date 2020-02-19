753f198011d15bbf9d62928d2a46857083383f3a
MACorman <madelinecorman@gmail.com>
2020-02-19 18:05:10 -0500

    created some components. got the app up and running with some text to the screen.

---
+++ containers/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+  } from 'react-native';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Nav Bar</Text>
+            </View>
+        )
+    }
+}
+
+export default NavBar
